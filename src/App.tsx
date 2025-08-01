3ffdd2125e83181525065bb42145bc4277038b47
Aleksandr Grechanik <Aleksandr.Grechanik@everise.com>
2025-08-01 17:17:12 +0300

    added loader for page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { routes } from './shared/router';
 import { Header } from './widgets/header/header';
+import { PageLoader } from './entities/loader/loader';
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Header />
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<PageLoader />}>
           <Routes>
             {routes.map((route, i) => (
               <Route key={i} {...route} />
